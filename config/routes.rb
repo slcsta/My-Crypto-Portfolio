6eb9c1ed9b9a2ffefa11e9497a96af09a3376ad5
Sarah <sarahlchavez@gmail.com>
2021-08-01 15:58:09 -0700

    Set the app homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#index'
+  
   resources :markets
   resources :trades
   resources :cryptos
   resources :users
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
