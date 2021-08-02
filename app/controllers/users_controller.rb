93e09e78458d8abedc6e254de45a0e419c0b3210
Sarah <sarahlchavez@gmail.com>
2021-08-02 05:52:51 -0700

    Added create user action for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,22 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to @user, alert: "successful sign-up"
+        else 
+            render :new, alert: "signup error, please try again"
+        end
     end
 
     def show
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
