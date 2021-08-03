ba53fd20bc8fb4b76cd9b20f7e65239a18a81bd7
Sarah <sarahlchavez@gmail.com>
2021-08-02 21:16:02 -0700

    Built out logout and login form and actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,25 @@ class SessionsController < ApplicationController
     end
 
     def create 
+        #byebug
+        #does the user exist in our database???
     # 1. search our database for the user entry that matches the username provided in the login form, 
-        @user = User.find_by(username: params[:username])
-    # 2. authenticate using bcrypt and the provided password
-        return head(:forbidden) unless @user.authenticate(params[:password])
-    # 3. setting the session[:user_id] and redirecting the user
-        session[:user_id] = @user.id 
+        @user = User.find_by(username: params[:user][:username]) #better to use find_by because we have to put a key in here
+    # 2. did we find the user and did they enter the correct password: authenticate using bcrypt and the provided password
+        if @user && @user.authenticate(params[:user][:password])
+    # 3. then set the session[:user_id] - 
+            session[:user_id] = @user.id 
     # 4. redirect to the user landing page
-        redirect_to user(@user)
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Error! Incorrect login info. Please try again."
+            redirect_to login_path
+        end
     end
 
     def destroy
-        
+        session.delete(:user_id)
+        redirect_to '/'
     end
 
 end
