1d548c8ad4d7dbe2ed9e4c6036ce71af472df86f
Sarah <sarahlchavez@gmail.com>
2022-03-30 04:31:12 -0700

    removed commented code from services folder

--- app/services/coingecko_api.rb
+++ app/services/coingecko_api.rb
@@ -2,10 +2,6 @@ require 'net/http'
 require 'json'
 
 class CoingeckoApi
-    # def initialize(crypto)
-    #     @crypto = crypto
-    #crypto_array = ["btc", "eth", "doge", "bch", "ltc", "etc""usdt", "xrp", "usdc", "dot", "uni", "busd", "link", "matic", "wbtc", "xlm", "icp", "bnb", "sol", "ada"]
-    # end
 
     def get_crypto_data
         crypto_array = ["btc", "eth", "doge", "bch", "ltc", "etc""usdt", "xrp", "usdc", "dot", "uni", "busd", "link", "matic", "wbtc", "xlm", "icp", "bnb", "sol", "ada"]
@@ -29,18 +25,6 @@ class CoingeckoApi
                 updated_at: Time.zone.now,
                 daily_change: data['price_change_percentage_24h']
             }
-        #     else 
-        #         return {
-        #             id: data['id'],
-        #             name: data['name'],
-        #             symbol: data['symbol'].upcase,
-        #             price: data['current_price'],
-        #             created_at: data['created_at'],
-        #             updated_at: data['updated_at'],
-        #             market_id: data['market_id'],
-        #             daily_change: data['price_change_percentage_24h']
-        #         }
-        #    end        
         }
         byebug
         Crypto.upsert_all(attributes)
